6f12d942a37187be99aa14dd573ff1624f790fa8
Vicky Kumari <vickykiitgaya@gmail.com>
2025-04-21 20:30:48 +0530

    first time commit

---
+++ src/main/java/com/example/gkeCICD/GkeCicdApplication.java
@@ -0,0 +1,14 @@
+package com.example.gkeCICD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class GkeCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GkeCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gkeCICD/GkeCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gkeCICD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GkeCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
